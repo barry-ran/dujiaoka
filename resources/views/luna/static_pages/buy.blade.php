c7a5bbdeba030acd1d3886bc3172929342c63c1a
rankun <870709864@qq.com>
2025-10-28 12:55:01 +0800

    feat: 英文版显示美元价格

--- resources/views/luna/static_pages/buy.blade.php
+++ resources/views/luna/static_pages/buy.blade.php
@@ -77,10 +77,10 @@
                                     </span>
                                 </div>
                                 <div class="price">
-                                    <span class="price-sign">￥</span>
-                                    <span class="price-num">{{ $actual_price }}</span>
+                                    <span class="price-sign">$</span>
+                                    <span class="price-num">{{ $actual_price > 6 ? ceil($actual_price / 6) : $actual_price }}</span>
                                     @if((int)$retail_price)
-                                        <span class="price-c">[<del>￥{{ $retail_price }}</del>]</span>
+                                        <span class="price-c">[<del>${{ $actual_price > 6 ? ceil($actual_price / 6) : $actual_price }}</del>]</span>
                                     @endif
                                 </div>
 
